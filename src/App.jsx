d767b33979634e4be98257a6fecbc16abdbfe25b
Oluwaseun2003 <oluwafemioluwaseun421@gmail.com>
2023-10-09 21:49:35 +0100

    Removed line 5 from App.jsx and line 11

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Clock from "./components/Clock";
-import EaNumber from "./components/Easection";
 
 function App () {
  return (
   <>
    <Header />,
    <Clock />,
-   <EaNumber />
    </>
   ); 
 }
